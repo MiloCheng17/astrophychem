86478686c939fa50badcbe478955c46999dc4593
Qianyi Cheng <qcheng1@log002.cm.cluster>
2019-04-09 15:34:10 -0500

    add running scripts

--- scripts/run.py
+++ scripts/run.py
@@ -3,13 +3,16 @@
 from coord_convert import *
 from File_info import *
 from fopt import *
+import sys
 
-ref_geom, keys = get_ref_geom('../ref.int')
+### need to have a ref.int file here
+ref_geom, keys = get_ref_geom('ref.int')
 geom_change, energy = get_geom_change('./')
-new_geom = get_new_geom(ref_geom,geom_change)
-#for key in range(len(keys)):
-#    print '%-7s %10.6f' % (keys[key],new_geom[key])
 
-inter_cart('../dummy','1.inp','1.inp-cart',new_geom,keys)
+# convert the angle to radius
+geom_change[1] = geom_change[1]*180.0/3.14
+new_geom = get_new_geom(ref_geom,geom_change)
+inter_cart('ref.int','1.inp','1.inp-cart',new_geom,keys)
 gangstrom_bohr('1.inp-cart','1.inp-cart.bohr')
+
 get_ff('./fort.9903')
