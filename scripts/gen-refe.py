b778f06c54f790c9daf4e0ad1ca45b06510302fe
Qianyi Cheng <qcheng1@log001.cm.cluster>
2019-04-25 15:03:32 -0500

    update gen-refe.py

---
+++ scripts/gen-refe.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+### Default sigfig of energy is 12 based on molpro output ###
+### usage: python gen-refe.py, need to have energy-old.dat ###
+
+from numpy import *
+
+
+olde = genfromtxt('energy-old.dat',usecols=-1)
+
+newe = olde - olde[0]
+
+savetxt('energy.dat',newe,fmt='%.12f')
